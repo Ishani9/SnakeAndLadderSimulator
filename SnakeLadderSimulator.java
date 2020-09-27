9328f4241a5acf971fdf7010e56436174211af0e
Ishani Mhatre <mhatreishani1999@gmail.com>
2020-09-28 00:13:53 +0530

    UC7 commited

--- SnakeLadderSimulator.java
+++ SnakeLadderSimulator.java
@@ -1,35 +1,61 @@
 public class SnakeLadderSimulator{
-	public static void main(String[] args) {
-		int start = 0;
-		int count=0;
-		int check;
-		
-		//No play=0; Ladder=1; Snake=2
-		do {
-			count++;
-			int num = (int)Math.floor(Math.random()*10)%6 + 1;
-			check= (int) Math.floor (Math.random() * 10) % 3;
-			switch (check) {
-				case 0:			// no play
-					start+= 0;
-					break;
-				case 1:			//ladder
-					start+=num;
-					break;
-				case 2:			//snake
-					start-=num;
-			}
-			if(start>100)
-				start-=num;
-			else if(start<0)
-				start=0;
-			else if (start == 100){
-				System.out.println("Position: "+ start);
-				System.out.println("No. of times played: "+count);
-				break;
-			}
-			System.out.println("Position: "+ start);
-		}
-		while(start!=100);		
-	}
+    static boolean turndecider=true;
+    
+    public static int diceSimulator (int position,String Player){
+    	   final int NO_PLAY=0;
+	       final int LADDER=1;
+	       final int SNAKE=2;  
+	       
+    	       int  num = (int)Math.floor(Math.random()*10)%6 + 1;
+    	       int option=(int) Math.floor (Math.random() * 10) % 3;
+                   switch(option) {
+            
+                      case NO_PLAY:
+                      break;
+            
+                      case LADDER:
+                      position=position + num;
+                      if(position>100)
+                      {
+                         position=position - num;
+                      }
+                      diceSimulator (position,Player);
+                      break;
+            
+                      case SNAKE:
+                      position=position - num;
+                      if(position<0)
+                      {
+                         position=0;
+                      }
+                      break;
+                   }
+                   System.out.println("Position of "+ Player +" "+ position);
+                   return position;
+    }
+
+
+        public static void main(String[] args){
+	          
+	       int positionOfP1=0;
+	       int positionOfP2=0;
+	       while(positionOfP1<100 && positionOfP2<100) {
+                   if(turndecider) {
+        	          positionOfP1=diceSimulator(positionOfP1,"Player 1");
+                   }
+                   else
+                   {
+        	          positionOfP2=diceSimulator(positionOfP2,"Player 2");
+                   }
+	           turndecider=!turndecider;
+	       }
+	       if(positionOfP1==100)
+	       {
+	    	   System.out.println("Player 1 wins");
+	       }
+	       else
+	       {
+	    	   System.out.println("Player 2 wins");
+	       }
+	}	        
 }
