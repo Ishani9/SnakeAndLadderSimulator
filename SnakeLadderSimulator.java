d63abc89abf5a16518508a8262a4ccff1e4a73c3
Ishani Mhatre <mhatreishani1999@gmail.com>
2020-09-28 00:06:13 +0530

    UC6 commited

--- SnakeLadderSimulator.java
+++ SnakeLadderSimulator.java
@@ -1,10 +1,13 @@
 public class SnakeLadderSimulator{
 	public static void main(String[] args) {
 		int start = 0;
+		int count=0;
+		int check;
+		
 		//No play=0; Ladder=1; Snake=2
 		do {
+			count++;
 			int num = (int)Math.floor(Math.random()*10)%6 + 1;
-			int check;
 			check= (int) Math.floor (Math.random() * 10) % 3;
 			switch (check) {
 				case 0:			// no play
@@ -22,10 +25,11 @@ public class SnakeLadderSimulator{
 				start=0;
 			else if (start == 100){
 				System.out.println("Position: "+ start);
+				System.out.println("No. of times played: "+count);
 				break;
 			}
-			System.out.println("Position:" + start);		
-		}		
-		while(start!=100);	
+			System.out.println("Position: "+ start);
+		}
+		while(start!=100);		
 	}
 }
